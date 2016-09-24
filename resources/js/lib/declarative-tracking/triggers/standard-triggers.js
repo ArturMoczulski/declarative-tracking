1b8b264a353dc21d5a517e3c8bcce7e4ce40fd35
Artur Moczulski <artur.moczulski@gmail.com>
2016-09-23 18:18:30 -0700

    add other jquery based triggers

--- resources/js/lib/declarative-tracking/triggers/standard-triggers.js
+++ resources/js/lib/declarative-tracking/triggers/standard-triggers.js
@@ -7,6 +7,22 @@ define(['require', '../utils/jquery-helper'],
 	        trigger: function(element, tracker) { jqHelper.bind(element, tracker, 'click') }
     	},
     	{
+	        name: 'change',
+	        trigger: function(element, tracker) { jqHelper.bind(element, tracker, 'change') }
+    	},
+    	{
+	        name: 'mouseenter',
+	        trigger: function(element, tracker) { jqHelper.bind(element, tracker, 'mouseenter') }
+    	},
+    	{
+	        name: 'select',
+	        trigger: function(element, tracker) { jqHelper.bind(element, tracker, 'select') }
+    	},
+    	{
+	        name: 'submit',
+	        trigger: function(element, tracker) { jqHelper.bind(element, tracker, 'submit') }
+    	},
+    	{
     		name: 'jquery',
 	        trigger: function(element, tracker) { 
 	        	var jqEventType = element.attr('data-track-jq-event-type')
--- test/declarative-tracking-tests/triggers/standard-triggers.js
+++ test/declarative-tracking-tests/triggers/standard-triggers.js
@@ -36,4 +36,109 @@ define(function(require) {
 	    assert.expect(1)
 
 	  })
+
+	  QUnit.test('trigger: change', function(assert) {
+
+	    var stubTrackService = function() { assert.ok(true, "stub-track-service fired") },
+	        element;
+
+	    (function(parent) {
+
+	      element = $("<input data-track='change' data-track-service='stub-track-service' />")
+	      parent.append(element)
+
+	    })($('#qunit-fixture'))
+
+	    DeclarativeTracking.registerTracker('stub-track-service', stubTrackService)
+	    DeclarativeTracking.bindTrackers()
+
+	    element.change()
+
+	    assert.expect(1)
+
+	  })
+
+	  QUnit.test('trigger: mouseenter', function(assert) {
+
+	    var stubTrackService = function() { assert.ok(true, "stub-track-service fired") },
+	        element;
+
+	    (function(parent) {
+
+	      element = $("<input data-track='mouseenter' data-track-service='stub-track-service' />")
+	      parent.append(element)
+
+	    })($('#qunit-fixture'))
+
+	    DeclarativeTracking.registerTracker('stub-track-service', stubTrackService)
+	    DeclarativeTracking.bindTrackers()
+
+	    element.mouseenter()
+
+	    assert.expect(1)
+
+	  })
+
+	  QUnit.test('trigger: select', function(assert) {
+
+	    var stubTrackService = function() { assert.ok(true, "stub-track-service fired") },
+	        element;
+
+	    (function(parent) {
+
+	      element = $("<select data-track='select' data-track-service='stub-track-service'></select>")
+	      parent.append(element)
+
+	    })($('#qunit-fixture'))
+
+	    DeclarativeTracking.registerTracker('stub-track-service', stubTrackService)
+	    DeclarativeTracking.bindTrackers()
+
+	    element.select()
+
+	    assert.expect(1)
+
+	  })
+
+	  QUnit.test('trigger: submit', function(assert) {
+
+	    var stubTrackService = function() { assert.ok(true, "stub-track-service fired") },
+	        element;
+
+	    (function(parent) {
+
+	      element = $("<input data-track='submit' data-track-service='stub-track-service' />")
+	      parent.append(element)
+
+	    })($('#qunit-fixture'))
+
+	    DeclarativeTracking.registerTracker('stub-track-service', stubTrackService)
+	    DeclarativeTracking.bindTrackers()
+
+	    element.submit()
+
+	    assert.expect(1)
+
+	  })
+
+	  QUnit.test('trigger: jquery', function(assert) {
+
+	    var stubTrackService = function() { assert.ok(true, "stub-track-service fired") },
+	        element;
+
+	    (function(parent) {
+
+	      element = $("<a data-track='jquery' data-track-jq-event-type='dblclick' data-track-service='stub-track-service'></a>")
+	      parent.append(element)
+
+	    })($('#qunit-fixture'))
+
+	    DeclarativeTracking.registerTracker('stub-track-service', stubTrackService)
+	    DeclarativeTracking.bindTrackers()
+
+	    element.dblclick()
+
+	    assert.expect(1)
+
+	  })
 })
