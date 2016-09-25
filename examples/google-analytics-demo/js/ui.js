d8e459c4a3c5d953233727e9bad436ddadb34d33
Artur Moczulski <artur.moczulski@gmail.com>
2016-09-25 14:18:13 +0000

    more ui refactoring

--- examples/google-analytics-demo/js/ui.js
+++ examples/google-analytics-demo/js/ui.js
@@ -21,57 +21,65 @@ define(['declarative-tracking/utils/jquery-helper',
             console.debug('Created new GA tracker: '+newTrackerName)
             
         },
-        gaExamplesUi = {
-            showDataPreviewInstructions: function() {
-                $("div#ga-switch-tracking-id-success, div#ga-tracking-overview-instructions").toggleClass('hidden')
-            },
-            updateUserGATrackingId: function() {
-                $('.ga-tracking-id').html(userGATrackingId)
-            },
-            updateSwitchUserGATrackingButton: function() {
-                var newTrackingId = $("input#ga-tracking-id").val()
-              
-                if(!newTrackingId || newTrackingId == GATrackingId || newTrackingId == userGATrackingId) {
-                    $("button#ga-switch-tracking-id").prop('disabled', true)
-                } else {
-                    $("button#ga-switch-tracking-id").prop('disabled', false)
-                }
+        gaExamplesUi = function() {
+            
+            var ui = { components: {} },
+                comps = ui.components
+            
+            comps.switchTrackingIdSuccessMessage = $("div#ga-switch-tracking-id-success")
+            comps.trackingPreviewInstructions = $("div#ga-tracking-overview-instructions")
+            comps.userTrackingIdDisplay = $('.ga-tracking-id')
+            comps.userTrackingIdInput = $("input#ga-tracking-id")
+            comps.switchTrackingIdButton = $("button#ga-switch-tracking-id")
+            
+            
+            comps.userTrackingIdInput.keyup(function() {
+            
+                comps.switchTrackingIdButton.update()
                 
+            })
+            
+            comps.switchTrackingIdButton.click(function() {
+                
+                // Get the new tracking id from the input box
+                var newTrackingId = comps.userTrackingIdInput.val()
+                
+                // Ignore if the user tracking id didn't change
+                if(newTrackingId == GATrackingId || newTrackingId == userGATrackingId) {
+                    return false;
+                }
+              
+                updateUserGATracker(newTrackingId)
+                
+                comps.trackingPreviewInstructions.toggleClass('hidden')
+                comps.switchTrackingIdButton.toggleClass('hidden')
+                
+                comps.userTrackingIdDisplay.update()
+                
+                comps.switchTrackingIdButton.update()
+              
+                return false;
+            })
+            
+            comps.switchTrackingIdButton.update = function() {
+                var newTrackingId = comps.userTrackingIdInput.val()
+                  
+                if(!newTrackingId || newTrackingId == GATrackingId || newTrackingId == userGATrackingId) {
+                    comps.switchTrackingIdButton.prop('disabled', true)
+                } else {
+                    comps.switchTrackingIdButton.prop('disabled', false)
+                }
             }
+            
+            comps.userTrackingIdDisplay.update = function() { $(this).html(userGATrackingId) }
+            
+            return ui
+            
         }
     
     return function() {
         
-        /**
-         * Switch Google Analytics Account button
-         */
-        $("button#ga-switch-tracking-id").click(function() {
-            
-            // Get the new tracking id from the input box
-            var newTrackingId = $("input#ga-tracking-id").val()
-            
-            // Ignore if the user tracking id didn't change
-            if(newTrackingId == GATrackingId || newTrackingId == userGATrackingId) {
-                return false;
-            }
-          
-            updateUserGATracker(newTrackingId)
-            
-            gaExamplesUi.showDataPreviewInstructions()
-            gaExamplesUi.updateUserGATrackingId()
-            gaExamplesUi.updateSwitchUserGATrackingButton()
-          
-            return false;
-        })
-        
-        /**
-         * Dynamic disabling of the button as you type GA Tracking Id
-         */
-        $("input#ga-tracking-id").keyup(function() {
-            
-            gaExamplesUi.updateSwitchUserGATrackingButton()
-            
-        })
+        gaExamplesUi()
         
     }
     
